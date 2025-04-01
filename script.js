48d16194b75dabfb8f9106bdd97a682acd5437ab
Devanshu07R <devanshudasgupta142004@gmail.com>
2025-04-01 23:20:20 +0530

    Initial commit - Animated-Login-page

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
